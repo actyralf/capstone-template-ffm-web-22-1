4ec15aba4de9bb3e3e53566b21cdf150a0b22ffd
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-11-25 16:03:47 +0100

    change text on home

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 export default function Home() {
-  return <h1>Willkommen zum 🐙-Capstone-Template!</h1>;
+  // return <h1>Willkommen zum 🐙-Capstone-Template!</h1>;
+  return <h1>Das ist meine tolle App</h1>;
 }
