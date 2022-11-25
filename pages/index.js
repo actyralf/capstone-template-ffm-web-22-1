89b174f9524baea3aed3964705b370f37da13d0b
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-11-25 01:27:02 +0100

    test actions with failing test run

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 export default function Home() {
-  return <h1>Willkommen zum 🐙-Capstone-Template!</h1>;
+  // return <h1>Willkommen zum 🐙-Capstone-Template!</h1>;
+  return <h1>Willkommen zum -Capstone-Template!</h1>;
 }
