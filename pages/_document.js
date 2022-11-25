c8defd2bcc0bb46c81803eddba2c9364b7dc8783
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-11-25 01:09:18 +0100

    change favicon to octopus

--- pages/_document.js
+++ pages/_document.js
@@ -6,15 +6,7 @@ export default function Document() {
       <Head>
         <link
           rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐙</text></svg>"
         />
       </Head>
       <body>
