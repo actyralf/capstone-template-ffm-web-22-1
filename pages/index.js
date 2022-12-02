54f2dc4a4ad1d12f3dfd1f36405c2834ba2aa55f
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-12-02 10:48:32 +0100

    update homepage (index.js)

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,12 @@ import styled from "styled-components";
 export default function Home() {
   return (
     <StyledContainer>
-      <h1>Herzlich willkommen zum 🐙-Capstone-Template!</h1>
+      <h1>Willkommen zum 🐙-Capstone-Template!</h1>
     </StyledContainer>
   );
 }
 
 const StyledContainer = styled.section`
-  width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
